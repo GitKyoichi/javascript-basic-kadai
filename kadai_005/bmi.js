72b3e05d705e6d1d9ac1503bfe2b4f440596d19b
GitKyoichi <ggltokuda@gmail.com>
2025-01-11 19:55:56 +0900

    kadai_005アップロード

---
+++ kadai_005/bmi.js
@@ -0,0 +1,11 @@
+// 体重(kg)
+let bodyWeight = 68;
+
+// 身長(m)
+let bodyHeight = 1.7;
+
+// BMIの計算
+let bodyMassIndex = bodyWeight / ( bodyHeight * bodyHeight );
+
+// コンソールへの出力
+console.log(bodyMassIndex);
